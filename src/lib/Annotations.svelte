25bee036ad648eb3dd13285316c9b9f994ea4e1d
mhkeller <code@mhkeller.com>
2024-09-17 22:17:51 -0400

    add annotations export

---
+++ src/lib/Annotations.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Editor from './Editor.svelte';
+	import Static from './Static.svelte';
+
+	let { edit, annotations } = $props();
+</script>
+
+{#if edit}
+	<Editor {annotations} />
+{:else}
+	<Static {annotations} />
+{/if}
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,2 +1,3 @@
 export { default as AnnotationsEditor } from './Editor.svelte';
 export { default as AnnotationsStatic } from './Static.svelte';
+export { default as Annotations } from './Annotations.svelte';
