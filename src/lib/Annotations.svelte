5b844def44b9902179796673639a7881be24fb5d
mhkeller <code@mhkeller.com>
2025-12-21 11:50:08 -0500

    feat: add Annotations wrapper component with editable prop
    - New Annotations.svelte that switches between Editor/Static based on 'editable' prop
    - Export types (Annotation, Arrow, ArrowSource, ArrowTarget) from index.js
    - Add ./types export path in package.json

---
+++ src/lib/Annotations.svelte
@@ -0,0 +1,15 @@
+<script>
+	/** @typedef {import('./types.js').Annotation} Annotation */
+
+	import Editor from './Editor.svelte';
+	import Static from './Static.svelte';
+
+	/** @type {{ annotations?: Annotation[], editable?: boolean }} */
+	let { annotations = $bindable([]), editable = true } = $props();
+</script>
+
+{#if editable}
+	<Editor bind:annotations />
+{:else}
+	<Static {annotations} />
+{/if}
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,2 +1,11 @@
+export { default as Annotations } from './Annotations.svelte';
 export { default as AnnotationsEditor } from './Editor.svelte';
 export { default as AnnotationsStatic } from './Static.svelte';
+
+// Re-export types for consumers
+/**
+ * @typedef {import('./types.js').Annotation} Annotation
+ * @typedef {import('./types.js').Arrow} Arrow
+ * @typedef {import('./types.js').ArrowSource} ArrowSource
+ * @typedef {import('./types.js').ArrowTarget} ArrowTarget
+ */
